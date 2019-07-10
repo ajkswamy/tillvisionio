8df98b4b0c2b5dc5b67cbf61bcbbb02e3d9e8b1b
Ajayrama Kumaraswamy <ajayrama.kumaraswamy@uni-konstanz.de>
2019-07-10 16:21:53 +0200

    added feature to get measurements and signals separately; other bug fixes
    added a test and associated test files for getting measurements and signals separately
    data parsed from log file is converted to numeric where possible
    empty strings are replaced by NaNs
    partially implemented getting image data based on label
    added requirements to setup.py
    bug fix in VWS_LOG_FSM_TEMPLATE.txt

--- setup.py
+++ setup.py
@@ -7,7 +7,10 @@ setup(
     package_data={"tillvisionio": ["textfsm_templates/*.txt"]},
     install_requires=[
                       "textfsm>=0.4.1",
-                      "numpy>=1.16.3"
+                      "numpy>=1.16.3",
+                      "pandas>=0.24.2",
+                      "xlrd>=1.2.0",
+                      "xlwt>=1.3.0"
                       ],
     python_requires=">=3.7"
 )
--- tillvisionio/vws.py
+++ tillvisionio/vws.py
@@ -1,11 +1,50 @@
 import textfsm
 import pkg_resources
 import pandas as pd
+import numpy as np
 
 
 def get_vws_log_template_filename():
     return pkg_resources.resource_filename('tillvisionio',
                                            "textfsm_templates/VWS_LOG_FSM_TEMPLATE.txt")
+
+
+def is_snapshot(row_entry: pd.Series) -> bool:
+    return row_entry["Timing_ms"].values == "(No calibration available)"
+
+
+def load_pst(filename):
+    """
+    Implementation based on legacy code by Giovanni Galizia and Georg Raiser
+    read tillvision based .pst files as uint16.
+    """
+    # filename can have an extension (e.g. .pst), or not
+    # reading stack size from inf
+    #inf_path = os.path.splitext(filename)[0] + '.inf'
+    #this does not work for /data/030725bR.pst\\dbb10F, remove extension by hand,
+    #assuming it is exactly 3 elements
+    if filename[-4] == '.':
+        filename = filename[:-4] #reomove extension
+    meta = {}
+    with open(filename+'.inf','r') as fh:
+    #    fh.next()
+        for line in fh.readlines():
+            try:
+                k,v = line.strip().split('=')
+                meta[k] = v
+            except:
+                pass
+    # reading stack from pst
+    shape = np.int32((meta['Width'],meta['Height'],meta['Frames']))
+    raw   = np.fromfile(filename+'.pst',dtype='int16')
+    data  = np.reshape(raw,shape,order='F')
+
+    # was swapping x, y axes; commented out to retain original order
+    # data  = data.swapaxes(0,1)
+
+    data  = data.astype('uint16')
+    return data
+
 
 class VWSDataManager(object):
 
@@ -20,14 +59,25 @@ class VWSDataManager(object):
             textfsm_template = textfsm.TextFSM(template_fle)
             data = textfsm_template.ParseText(vws_log_text)
             self.data_df = pd.DataFrame(columns=textfsm_template.header, data=data)
+            self.data_df.replace("", np.NaN, inplace=True)
+            self.data_df = self.data_df.apply(pd.to_numeric, errors='ignore')
 
     def get_measurement_metadata(self):
 
-        pass
+        return self.data_df.loc[lambda s: ~is_snapshot(s), :]
 
     def get_snapshot_metadata(self):
 
-        pass
+        return self.data_df.loc[lambda s: is_snapshot(s), :]
+
+    def get_image_data(self, label):
+
+        subset_matching_label = self.data_df.loc[lambda s: s["Label"] == label, :]
+
+        assert subset_matching_label.shape[0] == 1, f"More than one entries found in {self.vws_log_file}" \
+                                                    f"with label={label}"
+
+        subset_matching_label
 
 
 
