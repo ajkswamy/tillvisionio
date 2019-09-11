acb1404fefedb73c4a22bba998b9a8cdd549bdd9
Ajayrama Kumaraswamy <ajayrama.kumaraswamy@uni-konstanz.de>
2019-09-11 15:51:35 +0200

    refactored setup.py to make configurations a dictionary

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
-setup(
-    name="tillvisionio",
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
-    packages=find_packages(exclude=["^\."]),
-    package_data={"tillvisionio": ["textfsm_templates/*.txt"]},
-    install_requires=[
+config = {
+    "name": "tillvisionio",
+    "use_scm_version": True,
+    "setup_requires": ['setuptools_scm'],
+    "packages": find_packages(exclude=["^\."]),
+    "package_data": {"tillvisionio": ["textfsm_templates/*.txt"]},
+    "install_requires":[
                       "textfsm>=0.4.1",
                       "numpy>=1.16.3",
                       "pandas>=0.24.2",
@@ -13,5 +13,7 @@ setup(
                       "xlwt>=1.3.0",
                       "scipy>=1.3.0"
                       ],
-    python_requires=">=3.7"
-)
+    "python_requires": ">=3.7"}
+
+setup(**config)
+
